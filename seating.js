f9e2cc71e4f42bffb01639bce33812af50392777
John Rowley <outsidethebox@eircom.net>
2021-11-18 17:18:24 +0000

    seating updates

--- seating.js
+++ seating.js
@@ -5,9 +5,18 @@ $(function () {
 function initJquerySeating() {
     console.log(' i am jquery loading');
 
-    const maxSeatsChosen = 2;
+    const maxSeatsChosen = 4;
 
     $('.seat').on('click', function () {
+
+        const rowSeat = $(this).data("row");
+        const colSeat = $(this).data("col");
+
+        console.log(`You chose row ${rowSeat} and col ${colSeat}`)
+
+
+
+
 
         if ($(this).hasClass('reserved')) return;
 
@@ -15,21 +24,44 @@ function initJquerySeating() {
 
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
-return;
-        } 
+            ShowSeatCount();
+            return;
+        }
 
-        if ($('.selected').length >= maxSeatsChosen) { 
-            return; 
-        } 
+        if ($('.selected').length >= maxSeatsChosen) {
+            return;
+        }
 
 
         $(this).addClass('selected');
+        ShowSeatCount();
 
     })
 
 
 
 
+
+
+}
+
+function ShowSeatCount() {
+
+    let seatsChosen = [];
+
+    $('.selected').each(function (index) {
+
+        const rowSeat = $(this).data("row");
+        const colSeat = $(this).data("col");
+
+        seatsChosen.push({ row: rowSeat, col: colSeat });
+
+
+
+    })
+    let chosenSeats = seatsChosen.map(x => { return `${x.row}:${x.col}` });
+
+    $('#seatsChosen').html(`selected seats: ${chosenSeats.join(', ')}`);
 
 
 }
